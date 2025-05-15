87137ec0321d0441694fb97f0565758071c6cb5d
saumil <saumilpatel056@gmail.com>
2025-05-14 23:09:49 -0400

    call variables

--- week 2/index.php
+++ week 2/index.php
@@ -4,12 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>week 2</title>
 </head>
 <body>
-    <?php
-echo "hello world!";
+    <!-- learning echo -->
+<?php
+
+echo "<h1>hello world!</h1>";
+echo "<p>i am sam patel</p>";
 ?>
+<hr>
+    <!-- learning variables -->
+
+    <?php
+    $fname = "sam";
+    $lname = "patel";
+    
+    echo "<h1>hello "  .$fname. " ".$lname. "</h1>";
+    
+    ?>
+    
+    
+    
+
 
 </body>
 </html>
